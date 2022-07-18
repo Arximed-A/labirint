56bbcec3f5f841b145d6809dcebcb9c22930ee6a
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-07-18 09:31:03 +0300

    init

---
+++ src/script/script.js
@@ -0,0 +1,19 @@
+const app = {
+	moreInfo: null,
+	popupWindow: null,
+	button: null,
+	init() {
+		this.moreInfo = document.getElementById('moreInfo');
+		this.popupWindow = document.getElementById('popup');
+		this.button = document.getElementById('btn');
+		this.moreInfo.addEventListener('click', this.showPopap.bind(this));
+		this.button.addEventListener('click', this.showPopap.bind(this));
+	},
+	showPopap() {
+		this.popupWindow.classList.toggle('active');
+	},
+};
+
+app.init();
+
+
