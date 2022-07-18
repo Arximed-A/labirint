ff20e955347158b12bbd1e7365fdc26d8d1617bd
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-07-18 10:16:43 +0300

    почистил код

--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,17 @@
 const app = {
-	moreInfo: null,
-	popupWindow: null,
-	button: null,
-	init() {
-		this.moreInfo = document.getElementById('moreInfo');
-		this.popupWindow = document.getElementById('popup');
-		this.button = document.getElementById('btn');
-		this.moreInfo.addEventListener('click', this.showPopap.bind(this));
-		this.button.addEventListener('click', this.showPopap.bind(this));
-	},
-	showPopap() {
-		this.popupWindow.classList.toggle('active');
-	},
+  moreInfo: null,
+  popupWindow: null,
+  button: null,
+  init() {
+    this.moreInfo = document.getElementById('moreInfo');
+    this.popupWindow = document.getElementById('popup');
+    this.button = document.getElementById('btn');
+    this.moreInfo.addEventListener('click', this.showPopap.bind(this));
+    this.button.addEventListener('click', this.showPopap.bind(this));
+  },
+  showPopap() {
+    this.popupWindow.classList.toggle('active');
+  },
 };
 
 app.init();
